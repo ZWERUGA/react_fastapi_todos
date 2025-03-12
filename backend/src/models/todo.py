17911d830ea71ef157b2cc410e766d4b90b6e95c
Marat <maratnasyrov97@gmail.com>
2025-03-12 14:34:07 +0300

    Update todo models

--- backend/src/models/todo.py
+++ backend/src/models/todo.py
@@ -3,8 +3,22 @@ from sqlmodel import Field, SQLModel
 
 class TodoBase(SQLModel):
     text: str
-    is_completed: bool = Field(default=False)
 
 
 class Todo(TodoBase, table=True):
     id: int | None = Field(default=None, primary_key=True)
+    is_completed: bool = Field(default=False)
+
+
+class TodoPublic(TodoBase):
+    id: int
+    is_completed: bool
+
+
+class TodoCreate(TodoBase):
+    pass
+
+
+class TodoUpdate(TodoBase):
+    text: str | None = None
+    is_completed: bool | None = None
