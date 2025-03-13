0a5425c393fc41b11911df99fa39557cd6a8d4bb
Marat <maratnasyrov97@gmail.com>
2025-03-13 20:30:41 +0300

    Add User router and services

---
+++ backend/src/routers/users.py
@@ -0,0 +1,71 @@
+from datetime import timedelta
+from typing import Annotated
+
+from fastapi import APIRouter, Depends, HTTPException, status
+from fastapi.security import OAuth2PasswordRequestForm
+
+from models.users import User
+from models.tokens import Token
+from services.users import (
+    authenticate_user,
+    create_access_token,
+    get_user,
+    get_password_hash,
+)
+from database.database import SessionDep
+
+
+ACCESS_TOKEN_EXPIRE_MINUTES = 30
+
+
+router = APIRouter(prefix="/auth")
+
+
+@router.post("/login")
+async def login_for_access_token(
+    form_data: Annotated[OAuth2PasswordRequestForm, Depends()], session: SessionDep
+) -> Token:
+    """
+    Аутентификация пользователя.
+    """
+    user = authenticate_user(
+        session=session, username=form_data.username, password=form_data.password
+    )
+
+    if not user:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Incorrect username or password",
+            headers={"WWW-Authenticate": "Bearer"},
+        )
+
+    access_token_expires = timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+    access_token = create_access_token(
+        data={"sub": user.username}, expires_delta=access_token_expires
+    )
+    return Token(access_token=access_token, token_type="bearer")
+
+
+@router.post("/register")
+async def register(
+    form_data: Annotated[OAuth2PasswordRequestForm, Depends()], session: SessionDep
+):
+    """
+    Регистрирует пользователя.
+    """
+    if get_user(form_data.username, session):
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail="The user with this username is already registered...",
+        )
+
+    new_user = {
+        "username": form_data.username,
+        "hashed_password": get_password_hash(form_data.password),
+    }
+
+    db_user = User.model_validate(new_user)
+    session.add(db_user)
+    session.commit()
+    session.refresh(db_user)
+    return db_user
---
+++ backend/src/services/users.py
@@ -0,0 +1,97 @@
+from typing import Annotated
+from datetime import datetime, timedelta, timezone
+
+import jwt
+from jwt.exceptions import InvalidTokenError
+from fastapi import Depends, HTTPException, status
+from fastapi.security import OAuth2PasswordBearer
+from passlib.context import CryptContext
+from sqlmodel import select
+
+from database.database import SessionDep
+from models.tokens import TokenData
+from models.users import User
+
+
+SECRET_KEY = "cb470a798b0a89e0e83890b076396fe937be39fbe84ec71e988d9dc8bc68f048"
+ALGORITHM = "HS256"
+
+pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="auth/login")
+
+
+def verify_password(plain_password: str, hashed_password: str):
+    """
+    Проверяет пароли, сохраненный и введенный.
+    """
+    return pwd_context.verify(plain_password, hashed_password)
+
+
+def get_password_hash(password: str):
+    """
+    Возвращает захэшированный пароль.
+    """
+    return pwd_context.hash(password)
+
+
+def get_user(username: str, session: SessionDep):
+    """
+    Возвращает пользователя из БД.
+    """
+    db_user = session.exec(select(User).filter_by(username=username)).first()
+    return db_user
+
+
+def authenticate_user(username: str, password: str, session: SessionDep):
+    """
+    Аутентифицирует пользователя.
+    """
+    user = get_user(username=username, session=session)
+    if not user:
+        return False
+    if not verify_password(password, user.hashed_password):
+        return False
+    return user
+
+
+def create_access_token(data: dict, expires_delta: timedelta | None = None):
+    """
+    Создает токен для аутентификации.
+    """
+    to_encode = data.copy()
+    if expires_delta:
+        expire = datetime.now(timezone.utc) + expires_delta
+    else:
+        expire = datetime.now(timezone.utc) + timedelta(minutes=15)
+    to_encode.update({"exp": expire})
+    encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
+    return encoded_jwt
+
+
+async def get_current_user(
+    token: Annotated[str, Depends(oauth2_scheme)], session: SessionDep
+):
+    """
+    Получение пользователя из JWT токена.
+    """
+    credentials_exception = HTTPException(
+        status_code=status.HTTP_401_UNAUTHORIZED,
+        detail="Could not validate credentials",
+        headers={"WWW-Authenticate": "Bearer"},
+    )
+
+    try:
+        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        username = payload.get("sub")
+        if username is None:
+            raise credentials_exception
+        token_data = TokenData(username=username)
+    except InvalidTokenError:
+        raise credentials_exception
+    user = get_user(session=session, username=token_data.username)
+
+    if user is None:
+        raise credentials_exception
+
+    return user
