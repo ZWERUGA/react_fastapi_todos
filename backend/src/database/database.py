dc45c8b133401deb118023adc17817bddbcdbf30
Marat <maratnasyrov97@gmail.com>
2025-03-16 22:01:33 +0300

    Change database.py for sqlalchemy

--- backend/src/database/database.py
+++ backend/src/database/database.py
@@ -1,18 +1,22 @@
 from typing import Annotated
 
 from fastapi import Depends
-from sqlmodel import SQLModel, Session, create_engine
+from sqlalchemy import create_engine, MetaData
+from sqlalchemy.orm import Session
+
+from models.todos import Todo
+from models.users import User
 
 
 sqlite_file_name = "todos.db"
 sqlite_url = f"sqlite:///{sqlite_file_name}"
 
-connect_args = {"check_same_thread": False}
-engine = create_engine(sqlite_url, connect_args=connect_args)
+engine = create_engine(sqlite_url, echo=True)
 
 
 def create_db_and_tables():
-    SQLModel.metadata.create_all(engine)
+    Todo.metadata.create_all(engine)
+    User.metadata.create_all(engine)
 
 
 def get_session():
