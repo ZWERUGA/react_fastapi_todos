0398d6152e6f55ef90f78900760f04095f708108
Marat <maratnasyrov97@gmail.com>
2025-03-16 22:27:02 +0300

    Add protection route

--- backend/src/main.py
+++ backend/src/main.py
@@ -1,5 +1,8 @@
-from fastapi import FastAPI
+from typing import Annotated
+from fastapi import Depends, FastAPI
 
+from services.users import get_current_user
+from schemas.users import User, UserPublic
 from database.database import create_db_and_tables
 from routers.todos import router as todos_router
 from routers.users import router as users_router
@@ -14,6 +17,13 @@ app.include_router(users_router)
 @app.on_event("startup")
 def on_startup():
     create_db_and_tables()
+
+
+@app.get("/users/me/", response_model=UserPublic)
+async def read_users_me(
+    current_user: Annotated[UserPublic, Depends(get_current_user)],
+):
+    return current_user
 
 
 if __name__ == "__main__":
